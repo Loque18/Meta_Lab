aa6873c4f628282cb1a9a43b431a485b66a20963
aramistamboulian961 <aramistamboulian961@gmail.com>
2022-11-14 20:50:04 +0200

    display flex not working

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,12 +5,18 @@
 import { getLayout as getPageTitleLayout } from 'src/layouts/page-title';
 import Navbar from 'src/components/commons/navbar';
 // import metaFooter from '@/components/AboutMeta';
+
+
+import Cards from 'src/components/commons/cards/Cards';
+import cardStyle from 'src/components/style.module.scss'
 import styles from './home.module.scss';
 
 
 const Home = () => {
-    const { bg, multipleColor, content, title, description, btnMain } = styles;
+    const { bg, multipleColor, content, title, description, btnMain, } = styles;
+    const { cardContainer, cardTitle, cardDescription, cardBox, cardContent } = cardStyle;
     return (
+
         <div className={bg}>
             <Navbar />
             <div className={content}>
@@ -26,8 +32,21 @@ const Home = () => {
                     strategy and long term goals.
                 </p>
                 <button type="button" className={btnMain}>Our Services</button>
+            {
+                Cards.map((cards) => {
+                    return (
+                        <div className={cardContainer}>
+                            <div className={cardBox}>
+                                <div className={cardContent}>
+                                    <h1 className={cardTitle}>{cards.title}</h1>
+                                    <p className={cardDescription}>{cards.cardDescription}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
             </div>
-            {/* <metaFooter /> */}
         </div>
     );
 };
--- src/components/commons/cards/Cards.js
+++ src/components/commons/cards/Cards.js
@@ -1,25 +1,22 @@
-import Image from "next/image";
-import style from 'src/components/style.module.scss'
-const Cards = () => {
-  const {cardContainer} = style;
-  return (
-    <div className={cardContainer}>
-      <div className="cardBox">
-        <div className="cardContent">
-          <Image />
-          <h1 className="">50+ Partners & Clients</h1>
-          <p>
-            Lorem ipsum dolor sit amet, <br />
-            consectetuer adipiscing elit, sed <br />
-            diam nonummy nibh euismod <br />
-            tincidunt ut laoreet dolore magna <br />
-            aliquam erat Lorem ipsum dolor sit <br />
-            amet, consectetuer adipiscing elit, <br />
-          </p>
-        </div>
-      </div>
-    </div>
-  )
-}
+const Cards = [
+  {
+    id: 1,
+    title: '50+ Partners & Clients',
+    cardDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat Lorem ipsum dolor sit amet, consectetuer adipiscing elit,'
+  },
 
+  {
+    id: 2,
+    title: '10+ Combined Experiences',
+    cardDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat Lorem ipsum dolor sit amet, consectetuer adipiscing elit,'
+  },
+
+  {
+    id: 3,
+    title: '100% Reliable',
+    cardDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat Lorem ipsum dolor sit amet, consectetuer adipiscing elit,'
+  },
+
+  
+]
 export default Cards;
