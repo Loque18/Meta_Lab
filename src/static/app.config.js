3eaae85bd99214b846c7df0e30302e78f0379ece
loque18 <madridx2010@gmail.com>
2022-12-08 16:03:37 -0500

    chore: changed a lot of things

--- src/static/app.config.js
+++ src/static/app.config.js
@@ -1,8 +1,7 @@
 const appConfig = {
     appName: 'Metalab',
     appVersion: '2.1.0',
-    description:
-        'A template build with nextJS to bootstrap projects, it also contains a set of tools and pre buit code to help developers.', // The meta description element provides a summary of a page's content that search engines include in search results.
+    description: "Metalab brings the newest Blockchain & web3 technology to your fingertips. It's your gateway to WEB3", // The meta description element provides a summary of a page's content that search engines include in search results.
 };
 
 export default appConfig;
